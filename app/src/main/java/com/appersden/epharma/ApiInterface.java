7b74510441087b6bbaed0cc2d3b9496103d281a5
rezwanrony <rezwanronyrony@gmail.com>
2018-12-25 18:05:53 +0600

    last commit

--- app/src/main/java/com/appersden/epharma/ApiInterface.java
+++ app/src/main/java/com/appersden/epharma/ApiInterface.java
@@ -1,16 +1,20 @@
 package com.appersden.epharma;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface ApiInterface {
 
-/*
     @POST("login")
     @FormUrlEncoded
-    Call<User> checkuser(@Field("username") String username,
-                         @Field("password") String password);
+    Call<RegisterResponse> checkuser(@Field("username") String username,
+                                     @Field("password") String password);
 
     @POST("signup")
     @FormUrlEncoded
-    Call<Register> userRegistration(@Field("firstname") String firstname,
+    Call<RegisterResponse> userRegistration(@Field("firstname") String firstname,
                                     @Field("lastname") String lastname,
                                     @Field("email") String email,
                                     @Field("password") String password,
@@ -20,6 +24,5 @@ public interface ApiInterface {
                                     @Field("phone") String phone,
                                     @Field("address") String address,
                                     @Field("type") int type);
-*/
 
 }
